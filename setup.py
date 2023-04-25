c1fa7d820356f71426c904743a69c6485aa0d9af
Thalia <xi1le7@gmail.com>
2023-04-24 20:28:22 -0400

    modif12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PDFmanipulator_package',
-    version='0.8',
+    version='0.9',
     packages=find_packages(),
     install_requires=['PyPDF2'],
     author='Thalia',
